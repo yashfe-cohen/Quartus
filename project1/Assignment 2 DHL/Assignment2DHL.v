c96d9d570183d82a197b36e93823eac3d47ad378
yashfe-cohen <cohen.yashfe@gmail.com>
2026-07-06 08:52:13 +0300

    Auto backup via fast_beckup.bat

--- project1/Assignment 2 DHL/Assignment2DHL.v
+++ project1/Assignment 2 DHL/Assignment2DHL.v
@@ -4,10 +4,12 @@ module Assignment2DHL(
     output out
 );
 
-// הבורר הלוגי שלנו
-assign out = (sel == 2'b00) ? in[0] :
-             (sel == 2'b01) ? in[1] :
-             (sel == 2'b10) ? in[2] :
-                              in[3];
+
+    // לוגיקת הבורר של המוקס
+    assign out = (sel == 2'b00) ? in[0] :
+                 (sel == 2'b01) ? in[1] :
+                 (sel == 2'b10) ? in[2] :
+                                  in[3];
+
 
 endmodule
