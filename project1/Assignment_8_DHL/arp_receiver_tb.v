a4cb440ab0078b4c5e0605e15962488aaec8a3fb
yashfe-cohen <cohen.yashfe@gmail.com>
2026-07-15 15:43:47 +0300

    doc && new

--- project1/Assignment_8_DHL/arp_receiver_tb.v
+++ project1/Assignment_8_DHL/arp_receiver_tb.v
@@ -3,7 +3,7 @@
 module tb_arp_rx();
 
     reg clk, rst;
-    reg [1:0] din;
+    reg [1:0] din = 2'b0;
     wire done;
     wire err;
     
@@ -43,7 +43,30 @@ module tb_arp_rx();
 
         @(posedge clk) din = 0; // stop
         
+        #100;
+		  
+		  
+		  pkt = 336'hFFFFFFFFFFFF_001122334455_0906_0001_0800_06_04_0001_001122334455_C0A80101_000000000000_0A64660C; 
+
+        clk = 0; rst = 1; din = 0;
+        #20 rst = 0; #15;
+
+        // send preamble + sfd
+        repeat(31) @(posedge clk) din = 2'b10; 
+        @(posedge clk) din = 2'b11; 
+
+        // push packet bits
+        for (i = 0; i < 168; i = i + 1) begin
+            @(posedge clk);
+            din = pkt[335:334];    
+            pkt = pkt << 2; 
+        end
+
+        @(posedge clk) din = 0; // stop
+        
         #100 $stop;
+		  
+		  
     end
 
 endmodule
